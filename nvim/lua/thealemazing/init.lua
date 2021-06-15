eb855c3019a0c00225f962fc78a72afd08254aa8
TheAlemazing <hello@thealemazing.com>
2021-06-15 08:13:57 +0700

    update lsp

--- nvim/lua/thealemazing/init.lua
+++ nvim/lua/thealemazing/init.lua
@@ -1,3 +1,9 @@
+-- https://github.com/nanotee/nvim-lua-guide#tips-2
+function _G.dump(...)
+    local objects = vim.tbl_map(vim.inspect, {...})
+    print(unpack(objects))
+end
+
 require('thealemazing.lspconfig')
 require('thealemazing.telescope')
 require('thealemazing.compe')
--- nvim/lua/thealemazing/lspconfig.lua
+++ nvim/lua/thealemazing/lspconfig.lua
@@ -1,66 +1,43 @@
-local lspconfig = require('lspconfig')
+local nvim_lsp = require('lspconfig')
 
-local map = function(type, key, value)
-  vim.api.nvim_set_keymap(type, key, value, {noremap = true, silent = true});
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    local opts = { noremap=true, silent=true }
+
+    --Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- common
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', '<Space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+
+    -- extras
+    buf_set_keymap('n', '<Space>rs', '<Cmd>LspRestart<CR>', opts)
+    buf_set_keymap('n', '<Space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<Space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
 end
 
--- common
-map('n','gD'         , '<cmd>lua vim.lsp.buf.declaration()<CR>')
-map('n','gd'         , '<cmd>lua vim.lsp.buf.definition()<CR>')
-map('n','gr'         , '<cmd>lua vim.lsp.buf.references()<CR>')
-map('n','gs'         , '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-map('n','gi'         , '<cmd>lua vim.lsp.buf.implementation()<CR>')
-map('n','gt'         , '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-map('n','K'          , '<cmd>lua vim.lsp.buf.hover()<CR>')
-
--- extras
-map('n','<Space>rs', '<cmd>LspRestart<CR>')
-map('n','<Space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-map('n','<Space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-map('n','<Space>dn' , '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-map('n','<Space>dp' , '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-map('n','<Space>ee' , '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
-map('n','<Space>gw' , '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-map('n','<Space>gW' , '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
-
--- Enable diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-  }
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        underline = true,
+        signs = true,
+    }
 )
 
--- rust analyzer
-lspconfig.rust_analyzer.setup{
-  on_attach = function(client)
-    print("Rust analyzer started.");
-  end,
-  settings = {
-    ["rust-analyzer"] = {
-      assist = {
-        importGranularity = "module",
-        importPrefix = "by_self",
-      },
-      cargo = {
-        loadOutDirsFromCheck = true
-      },
-      procMacro = {
-        enable = true
-      },
-    }
-  }
+-- JavaScript & TypeScript
+nvim_lsp.tsserver.setup {
+    on_attach = on_attach,
 }
-
--- tsserver setup
-lspconfig.tsserver.setup {
-  on_attach = function(client)
-    print("Typescript LSP started.");
-
-    client.config.flags.allow_incremental_sync = true
-    client.resolved_capabilities.document_formatting = false
-
-  end
-}
-
