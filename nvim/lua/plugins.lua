9002b5452d7808cad8f42d3b89c63a08fcf730ee
HoomanOnEarth <hello@thealemazing.com>
2022-03-10 18:44:35 +0700

    update indentation

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,66 +3,71 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-	fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-	vim.cmd 'packadd packer.nvim'
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
 end
 
 function M.load()
-	require('packer').startup(function()
+  require('packer').startup(function()
 
-		-- Undo history
-		use 'mbbill/undotree'
+    -- Undo history
+    use 'mbbill/undotree'
 
-		-- LSP
-		use 'neovim/nvim-lspconfig'
-		use 'williamboman/nvim-lsp-installer'
-		use 'nvim-lua/popup.nvim'
-		use 'nvim-lua/plenary.nvim'
+    -- LSP
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
 
-		-- snippet
-		use 'hrsh7th/cmp-vsnip'
-		use 'hrsh7th/vim-vsnip'
-		use 'rafamadriz/friendly-snippets'
+    -- snippet
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/vim-vsnip'
+    use 'rafamadriz/friendly-snippets'
 
-		-- Autocomplete
-		use 'hrsh7th/cmp-nvim-lsp'
-		use 'hrsh7th/cmp-buffer'
-		use 'hrsh7th/cmp-path'
-		use 'hrsh7th/cmp-cmdline'
+    -- Autocomplete
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
     use 'hrsh7th/cmp-nvim-lsp-signature-help'
-		use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/nvim-cmp'
 
-		-- Lsp completion icons
-		use 'onsails/lspkind-nvim'
+    -- Lsp completion icons
+    use 'onsails/lspkind-nvim'
 
-		-- Code formatting
-		use {
-			'prettier/vim-prettier',
-			run = 'pnpm install',
-			ft = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'css', 'graphql', 'markdown', 'html', 'json' }
-		}
+    -- Code formatting
+    use {
+      'prettier/vim-prettier',
+      run = 'pnpm install',
+      ft = {
+        'javascript', 'typescript',
+        'javascriptreact', 'typescriptreact',
+        'html', 'css', 'json',
+        'graphql', 'markdown'
+      }
+    }
 
-		-- Telescope
-		use 'nvim-telescope/telescope.nvim'
-		use 'nvim-telescope/telescope-fzy-native.nvim'
-		use 'jeetsukumaran/telescope-buffer-lines.nvim'
-		use 'danielpieper/telescope-tmuxinator.nvim'
+    -- Telescope
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-telescope/telescope-fzy-native.nvim'
+    use 'jeetsukumaran/telescope-buffer-lines.nvim'
+    use 'danielpieper/telescope-tmuxinator.nvim'
 
-		-- Treesitter
-		use 'nvim-treesitter/nvim-treesitter'
+    -- Treesitter
+    use 'nvim-treesitter/nvim-treesitter'
 
-		-- JSX
-		use 'MaxMEllon/vim-jsx-pretty'
-		use 'styled-components/vim-styled-components'
+    -- JSX
+    use 'MaxMEllon/vim-jsx-pretty'
+    use 'styled-components/vim-styled-components'
 
-		-- Status line
-		use 'nvim-lualine/lualine.nvim'
-		use 'kyazdani42/nvim-web-devicons'
+    -- Status line
+    use 'nvim-lualine/lualine.nvim'
+    use 'kyazdani42/nvim-web-devicons'
 
-		-- Theme
-		use 'rose-pine/neovim'
+    -- Theme
+    use 'rose-pine/neovim'
 
-	end)
+  end)
 end
 
 return M
