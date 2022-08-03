40ee8e147dc1862cec3d304842011fab4ede169d
HoomanOnEarth <hello@thealemazing.com>
2022-08-03 20:27:49 +0700

    add treesitter playground

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -104,4 +104,24 @@ require("nvim-treesitter.configs").setup {
             },
         },
     },
+
+    playground = {
+        enable = true,
+        updatetime = 25,
+        persist_queries = true,
+        keybindings = {
+            toggle_query_editor = "o",
+            toggle_hl_groups = "i",
+            toggle_injected_languages = "t",
+
+            -- This shows stuff like literal strings, commas, etc.
+            toggle_anonymous_nodes = "a",
+            toggle_language_display = "I",
+            focus_language = "f",
+            unfocus_language = "F",
+            update = "R",
+            goto_node = "<cr>",
+            show_help = "?",
+        },
+    },
 }
